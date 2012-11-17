48fb8f9231dbb050c20a68ede4a46be6636818f4
Eric Barendt <ebarendt@gmail.com>
2012-11-16 18:05:04 -0800

    First import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RssRepeater::Application.config.secret_token = '84f23d3333167a29f798d83593b2d797a7b8186df4e033405f805f526355c33467ebe5d709c97cbe2bc61e0467919909158d1b457358ab3a43ca27264da31d9d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RssRepeater::Application.config.session_store :cookie_store, key: '_rss_repeater_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RssRepeater::Application.config.session_store :active_record_store
